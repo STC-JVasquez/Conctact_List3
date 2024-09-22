f4cac4f946d59711cad58409be6601596627aa8b
Juan Vasquez <jvasqu53@stu.southtexascollege.edu>
2024-09-22 17:25:21 -0500

    Finished addcontact component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <script setup>
 import Header from './components/Header.vue';
+import AddContact from './components/AddContact.vue';
 </script>
 <template>
   <Header></Header>
+  <div>
+    <AddContact></AddContact>
+  </div>
 </template>
---
+++ src/components/AddContact.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+
+const name = ref('')
+const phone = ref('')
+
+const emit = defineEmits(['contactAdded'])
+
+const addContact = () => {
+    if(name.value.trim() === '' || phone.value.trim() === ''){
+        return
+    }
+    emit('contactAdded', {
+        name: name.value,
+        phone: phone.value,
+    })
+
+    name.value = ''
+    phone.value = ''
+}
+</script>
+<template>
+    <section class=" add-contact">
+        <h3>Add New Contact</h3>
+        <form @submit.prevent="addContact">
+            <input type="text" v-model="name" placeholder="Name"/>
+            <input type="text" v-model="phone" placeholder="Phone"/>
+            <button type="submit">Add Contact</button>
+        </form>
+    </section>
+</template>
