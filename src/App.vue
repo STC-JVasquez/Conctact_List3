bc95a2fa9b8307d830c06ec077bda30ea8954ecb
Juan Vasquez <jvasqu53@stu.southtexascollege.edu>
2024-09-22 18:51:38 -0500

    Finished the contactlist component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <script setup>
 import Header from './components/Header.vue';
 import AddContact from './components/AddContact.vue';
+import ContactList from './components/ContactList.vue';
+import { ref } from 'vue';
+
+const handleAddContact = (newContact) => {
+  contacts.value.push(newContact)
+}
+
+const contacts = ref([])
 </script>
 <template>
   <Header></Header>
   <div>
-    <AddContact></AddContact>
+    <AddContact @contactAdded="handleAddContact"></AddContact>
+    <ContactList :contacts="contacts" @contactDeleted="handleDeleteContact"></ContactList>
   </div>
 </template>
---
+++ src/components/ContactList.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+
+const props = defineProps({
+    contacts: {
+        type: Array,
+        Required: true,
+    }
+})
+
+const emit = defineEmits(['contactDeleted'])
+
+const deletContact = (id) => {
+    emit('contactDeleted', id)
+}
+
+
+</Script>
+<template>
+    <h3>Contact List</h3>
+    <ul id="list" class="list">
+        <li v-for="contact in contacts" :key="contact.id">
+            {{ contact.name }}: {{ contact.phone }}
+            <button @click="deletContact(contact.id)" class="delete-btn">Delete</button>
+        </li>
+    </ul>
+</template>
