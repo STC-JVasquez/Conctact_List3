f8ec821e972c3ba6910d646e954e57694aa9d4ac
Juan Vasquez <jvasqu53@stu.southtexascollege.edu>
2024-09-22 19:05:31 -0500

    Finished contact stats to keep track # of contacts

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Header from './components/Header.vue';
 import AddContact from './components/AddContact.vue';
 import ContactList from './components/ContactList.vue';
 import { ref } from 'vue';
+import ContactStats from './components/ContactStats.vue';
 
 const handleAddContact = (newContact) => {
   contacts.value.push(newContact)
@@ -14,6 +15,7 @@ const contacts = ref([])
   <Header></Header>
   <div>
     <AddContact @contactAdded="handleAddContact"></AddContact>
+    <ContactStats :contacts="contacts"></ContactStats>
     <ContactList :contacts="contacts" @contactDeleted="handleDeleteContact"></ContactList>
   </div>
 </template>
---
+++ src/components/ContactStats.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import {computed, defineProps} from 'vue';
+
+    const props = defineProps(['contacts'])
+    const totalContacts = computed(() => props.contacts.length)
+</script>
+<template>
+    <section class="contact-stats">
+        <h3>Number of Contacts</h3>
+        <p>Total Contacts: {{ totalContacts}}</p>
+    </section>
+</template>
