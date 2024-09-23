3609b83bef9d399f10cf6145e834b841d79e8e57
Juan Vasquez <jvasqu53@stu.southtexascollege.edu>
2024-09-22 21:05:26 -0500

    Updated CSS for better looks and added persistant storage

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Header from './components/Header.vue';
 import AddContact from './components/AddContact.vue';
 import ContactList from './components/ContactList.vue';
 import ContactStats from './components/ContactStats.vue';
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 
 const contacts = ref([])
 const currentContact = ref(null)
@@ -14,10 +14,12 @@ const handleAddContact = (contactData) => {
     name: contactData.name,
     phone: contactData.phone,
   })
+  saveToLocalStorage()
 }
 
 const handleDeleteContact = (id) => {
   contacts.value = contacts.value.filter((contact) => contact.id !== id)
+  saveToLocalStorage()
 }
 
 const handleEditContact = (contact) => {
@@ -30,13 +32,23 @@ const handleUpdateContact = (updatedContact) =>{
     contacts.value[index] = updatedContact
     currentContact.value = null
   }
+  saveToLocalStorage()
 }
 
 const generateID = () => {
   return Math.floor(Math.random()*10000000)
 }
 
+const saveToLocalStorage = () => {
+  localStorage.setItem('contacts', JSON.stringify(contacts.value))
+}
 
+onMounted(() => {
+  const savedContacts = JSON.parse(localStorage.getItem('contacts'))
+  if (savedContacts) {
+    contacts.value = savedContacts
+  }
+})
 
 </script>
 <template>
